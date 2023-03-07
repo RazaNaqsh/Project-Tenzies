5f9885dee78d6bdb4be28b253f9140dccd54e57c
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-07 08:58:48 +0530

    setup project with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<div className="App">
+			<h1>This is Vite React</h1>
+			<p>very fast</p>
+			<h2>Now i get it</h2>
+			<h3>this can be very fast</h3>
+			<p>reaaact</p>
+		</div>
+	);
+}
+
+export default App;
