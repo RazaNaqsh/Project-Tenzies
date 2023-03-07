7e5c488e02dc0d9b2fe04852c7a5039a5f4028a4
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-07 11:07:58 +0530

    Generate new Dice array

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
 import Heading from "./components/Heading";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+	// make boxes by mapping
+	// display random number
+	// roll button to display again
+	// hold state inside button
+	const [dice, setDice] = useState(newDiceArray());
+
+	function generateNewDie() {
+		const die = {
+			value: Math.ceil(Math.random() * 6),
+			id: nanoid(),
+		};
+		return die;
+	}
+
+	function newDiceArray() {
+		const diceArray = [];
+		for (let i = 0; i < 10; i++) {
+			diceArray.push(generateNewDie());
+		}
+		return diceArray;
+	}
+	console.log(dice);
 	return (
 		<div className="App">
 			<Heading />
